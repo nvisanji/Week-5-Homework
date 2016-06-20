38696e62144da43d18afaa6c4ab17cf9af3af523
nvisanji <nvisanj@nadeem-v.local>
2016-06-20 06:46:42 -0700

    Initial Commit

---
+++ Week 5 Homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week 5 Homework
+//
+//  Created by Nadeem Visanji on 6/20/16.
+//  Copyright © 2016 Nadeem Visanji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
